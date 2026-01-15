14fd770ad48dda9fa619ebbba146ce43748f5469
kantaphong <xooox1234xx@gmail.com>
2026-01-15 22:38:10 +0700

    fix: correct command

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # 1. ลง Docker และ Git
 sudo apt-get update -y
-sudo apt-get install -y docker.io git
+sudo apt-get install -y docker.io git docker-compose make
 
 # 2. เริ่ม Docker
 sudo systemctl start docker
@@ -9,7 +9,6 @@ sudo systemctl enable docker
 # 3. ดึงโค้ดทั้งโปรเจกต์ลงมา
 git clone https://github.com/Kantaphong007/SDA-minecraft-server-application /home/ubuntu/project
 
-# 4. เข้าไปสร้าง Docker Image และรัน
+# 4. Run container
 cd /home/ubuntu/project
-sudo docker build -t my-webapp .
-sudo docker run -d -p 80:80 --name webserver my-webapp
+sudo make deploy
